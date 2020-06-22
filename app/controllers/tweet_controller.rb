de6ac3b1039e7eb0446fa32db7d66fe3121869cb
callumknights <callum.knights@digital.cabinet-office.gov.uk>
2020-06-22 22:32:19 +0100

    Add create method to Tweet controller

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,2 +1,15 @@
 class TweetController < ApplicationController
+    def create
+      @tweet = Tweet.new(tweet_params)
+
+      @tweet.save
+      redirect_to @tweet
+    end
+
+    private
+
+    def tweet_params
+      params.require(:tweet).permit(:user_id, :content)
+    end
+
 end
